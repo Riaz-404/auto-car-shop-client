b0abc6070306c992d8553fea2829daf1f8355d0a
Riaz-404 <imriaz.cu@gmail.com>
2022-04-02 15:15:55 +0600

    car details card added

--- src/Pages/CarDetails/CarDetails/CarDetails.js
+++ src/Pages/CarDetails/CarDetails/CarDetails.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavigationBar from '../../Home/NavigationBar/NavigationBar';
+import Details from './Details/Details';
 
 const CarDetails = () => {
     return (
         <div>
             <NavigationBar/>
+            <Details/>
         </div>
     );
 };
---
+++ src/Pages/CarDetails/CarDetails/Details/Details.js
@@ -0,0 +1,181 @@
+import React, { useState } from 'react'
+import { StarIcon } from '@heroicons/react/solid'
+import { RadioGroup } from '@headlessui/react'
+
+import car1 from "../../../../Images/car-1.jpg";
+
+
+const cars = [
+    {
+        _id: 1,
+        name: 'VOLKSWAGEN SCIROCCO 2016',
+        price: '$20000',
+        image: car1,
+        fuel: 'Petrol',
+        deliveryTime: 'within a month',
+        colors: [
+            { name: 'White', class: 'bg-white', selectedClass: 'ring-gray-400' },
+            { name: 'Gray', class: 'bg-gray-200', selectedClass: 'ring-gray-400' },
+            { name: 'Black', class: 'bg-gray-900', selectedClass: 'ring-gray-900' },
+        ],
+    }
+]
+const reviews = { href: '#', average: 4, totalCount: 117 }
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
+
+const Details = () => {
+    const [selectedColor, setSelectedColor] = useState(cars[0].colors[0])
+
+    return (
+        <div className="bg-white">
+            <div className="pt-6">
+
+
+
+
+                {/* Product info */}
+                <div className="max-w-2xl mx-auto pt-10 pb-16 px-4 sm:px-6 lg:max-w-7xl lg:pt-16 lg:pb-24 lg:px-8 lg:grid lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8">
+
+                    {/* Image gallery */}
+
+                    <div className="mt-6 max-w-2xl mx-auto col-span-2">
+                        <div className="hidden rounded-lg overflow-hidden lg:block">
+                            <img
+                                src={cars[0].image}
+                                alt={cars[0].name}
+                                className="w-full h-full object-center object-cover"
+                            />
+                        </div>
+                    </div>
+
+
+                    {/* Options */}
+                    <div className="mt-4 lg:mt-0 lg:row-span-3">
+                        <h2 className="sr-only">Product information</h2>
+                        <div className="lg:col-span-2 lg:pr-8">
+                            <h1 className="text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">{cars[0].name}</h1>
+                        </div>
+                        <p className="text-3xl text-gray-900">{cars[0].price}</p>
+
+                        {/* Reviews */}
+                        <div className="mt-6">
+                            <h3 className="sr-only">Reviews</h3>
+                            <div className="flex items-center">
+                                <div className="flex items-center">
+                                    {[0, 1, 2, 3, 4].map((rating) => (
+                                        <StarIcon
+                                            key={rating}
+                                            className={classNames(
+                                                reviews.average > rating ? 'text-gray-900' : 'text-gray-200',
+                                                'h-5 w-5 flex-shrink-0'
+                                            )}
+                                            aria-hidden="true"
+                                        />
+                                    ))}
+                                </div>
+                                <p className="sr-only">{reviews.average} out of 5 stars</p>
+                                <a href={reviews.href} className="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500">
+                                    {reviews.totalCount} reviews
+                                </a>
+                            </div>
+                        </div>
+
+                        <form className="mt-4">
+                            {/* Colors */}
+                            <div>
+                                <h3 className="text-sm text-gray-900 font-medium">Color</h3>
+
+                                <RadioGroup value={selectedColor} onChange={setSelectedColor} className="mt-4">
+                                    <RadioGroup.Label className="sr-only">Choose a color</RadioGroup.Label>
+                                    <div className="flex items-center space-x-3">
+                                        {cars[0].colors.map((color) => (
+                                            <RadioGroup.Option
+                                                key={color.name}
+                                                value={color}
+                                                className={({ active, checked }) =>
+                                                    classNames(
+                                                        color.selectedClass,
+                                                        active && checked ? 'ring ring-offset-1' : '',
+                                                        !active && checked ? 'ring-2' : '',
+                                                        '-m-0.5 relative p-0.5 rounded-full flex items-center justify-center cursor-pointer focus:outline-none'
+                                                    )
+                                                }
+                                            >
+                                                <RadioGroup.Label as="p" className="sr-only">
+                                                    {color.name}
+                                                </RadioGroup.Label>
+                                                <span
+                                                    aria-hidden="true"
+                                                    className={classNames(
+                                                        color.class,
+                                                        'h-8 w-8 border border-black border-opacity-10 rounded-full'
+                                                    )}
+                                                />
+                                            </RadioGroup.Option>
+                                        ))}
+                                    </div>
+                                </RadioGroup>
+                            </div>
+
+                            {/* Sizes */}
+                            <div className="mt-10">
+                                <div className="flex items-center justify-between">
+                                    <h3 className="text-sm text-gray-900 font-medium">Pick A Delivery Date</h3>
+                                    
+                                </div>
+
+                                                         
+
+                            </div>
+
+                            <button
+                                type="submit"
+                                className="mt-10 w-full bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                            >
+                                Add to bag
+                            </button>
+                        </form>
+                    </div>
+
+                    {/* <div className="py-10 lg:pt-6 lg:pb-16 lg:col-start-1 lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
+                        
+                        <div>
+                            <h3 className="sr-only">Description</h3>
+
+                            <div className="space-y-6">
+                                <p className="text-base text-gray-900">{cars[0].description}</p>
+                            </div>
+                        </div>
+
+                        <div className="mt-10">
+                            <h3 className="text-sm font-medium text-gray-900">Highlights</h3>
+
+                            <div className="mt-4">
+                                <ul role="list" className="pl-4 list-disc text-sm space-y-2">
+                                    {cars[0].highlights.map((highlight) => (
+                                        <li key={highlight} className="text-gray-400">
+                                            <span className="text-gray-600">{highlight}</span>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div className="mt-10">
+                            <h2 className="text-sm font-medium text-gray-900">Details</h2>
+
+                            <div className="mt-4 space-y-6">
+                                <p className="text-sm text-gray-600">{cars[0].details}</p>
+                            </div>
+                        </div>
+                    </div> */}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Details;
