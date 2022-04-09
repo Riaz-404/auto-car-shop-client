d4463754b704bdd0c0cdfcf51ef70895571e0365
Riaz-404 <imriaz.cu@gmail.com>
2022-04-10 00:09:55 +0600

    icon added

--- src/Pages/Dashboard/SideBar/Sidebar.js
+++ src/Pages/Dashboard/SideBar/Sidebar.js
@@ -1,25 +1,42 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import useAuth from '../../../hooks/useAuth';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import PaymentIcon from '@mui/icons-material/Payment';
+import ReviewsIcon from '@mui/icons-material/Reviews';
+import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import ListAltIcon from '@mui/icons-material/ListAlt';
+import FactCheckIcon from '@mui/icons-material/FactCheck';
+
 
 const Sidebar = () => {
-    const {admin} = useAuth();
+    const { admin } = useAuth();
     console.log(admin);
 
     return (
         <div className='w-64 h-screen border-r-2'>
             <li className='py-2 list-none'>
                 <Link to="profile">
-                    <ol className='p-3 text-center cursor-pointer hover:bg-gray-700 hover:text-white'>Profile</ol>
+                    <ol className='p-3 text-left cursor-pointer hover:bg-gray-700 hover:text-white'>
+                        <AccountCircleIcon className='mx-2'/>Profile
+                    </ol>
                 </Link>
                 <Link to="myorders">
-                    <ol className='p-3 text-center cursor-pointer hover:bg-gray-700 hover:text-white'>My Orders</ol>
+                    <ol className='p-3 text-left cursor-pointer hover:bg-gray-700 hover:text-white'>
+                        <ShoppingCartIcon className='mx-2'/>My Orders
+                    </ol>
                 </Link>
                 <Link to="payment">
-                    <ol className='p-3 text-center cursor-pointer hover:bg-gray-700 hover:text-white '>Payment</ol>
+                    <ol className='p-3 text-left cursor-pointer hover:bg-gray-700 hover:text-white '>
+                        <PaymentIcon className='mx-2' />Payment
+                    </ol>
                 </Link>
                 <Link to="review">
-                    <ol className='p-3 text-center cursor-pointer hover:bg-gray-700 hover:text-white '>Review</ol>
+                    <ol className='p-3 text-left cursor-pointer hover:bg-gray-700 hover:text-white '>
+                        <ReviewsIcon className='mx-2' />Review
+                    </ol>
                 </Link>
             </li>
             {
@@ -28,16 +45,24 @@ const Sidebar = () => {
                         <hr />
                         <li className='py-2 list-none'>
                             <Link to="manageorders">
-                                <ol className='p-3 text-center cursor-pointer hover:bg-gray-700 hover:text-white'>Manage All Orders</ol>
+                                <ol className='p-3 text-left cursor-pointer hover:bg-gray-700 hover:text-white'>
+                                    <ListAltIcon className='mx-2' />Manage All Orders
+                                </ol>
                             </Link>
                             <Link to="addproduct">
-                                <ol className='p-3 text-center cursor-pointer hover:bg-gray-700 hover:text-white'>Add A Product</ol>
+                                <ol className='p-3 text-left cursor-pointer hover:bg-gray-700 hover:text-white'>
+                                    <AddShoppingCartIcon className='mx-2' />Add A Product
+                                </ol>
                             </Link>
                             <Link to="makeadmin">
-                                <ol className='p-3 text-center cursor-pointer hover:bg-gray-700 hover:text-white '>Make Admin</ol>
+                                <ol className='p-3 text-left cursor-pointer hover:bg-gray-700 hover:text-white '>
+                                    <ManageAccountsIcon className='mx-2'/>Make Admin
+                                </ol>
                             </Link>
                             <Link to="manageproduct">
-                                <ol className='p-3 text-center cursor-pointer hover:bg-gray-700 hover:text-white '>Manage Products</ol>
+                                <ol className='p-3 text-left cursor-pointer hover:bg-gray-700 hover:text-white '>
+                                    <FactCheckIcon className='mx-2'/>Manage Products
+                                </ol>
                             </Link>
                         </li>
                     </>
